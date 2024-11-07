c7fb3623f04f04c75993990dac8ff73e7e842f09
Ndagire Catherine <tronellahn@gmail.com>
2024-11-07 13:57:25 +0300

    commit

--- app/page.js
+++ app/page.js
@@ -3,6 +3,9 @@ import Title from "./Components/Title";
 import Content from "./Components/Content";
 import Button from '@mui/material/Button';
 
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
 function Home() {
 
 function goToNextPage(){}
@@ -23,6 +26,14 @@ function goToNextPage(){}
       <Button variant="text">Text</Button>
       < Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+
+      export default function BoxBasic() {
+  return (
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      The 2024 U.S. presidential election will take place on November 5, 2024. This election will determine the President and Vice President of the United States for the next four-year term, beginning in January 2025. The election process involves voters in all 50 states and the District of Columbia casting ballots for electors in the Electoral College, which formally elects the President and Vice President.
+    </Box>
+  );
+}
 
 
 
